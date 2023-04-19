23a1dc296200a240e77bd8b16099c00f76c49fa2
Marvin <marboynel@gmail.com>
2023-04-20 11:16:29 +1200

    removed reactpractise

--- REACTPRACTISE/core/events/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import Event
-
-
-class EventModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('id', 'name', 'description', 'qr_code', 'date')
