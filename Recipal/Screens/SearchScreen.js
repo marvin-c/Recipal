2908dcc10454e3c1a757b15abb2576fe702ce410
binibiningmayumi <rocellevaldez13@gmail.com>
2023-04-24 23:03:33 +1200

    Container for Search Screen

--- Recipal/Screens/SearchScreen.js
+++ Recipal/Screens/SearchScreen.js
@@ -1,33 +1,64 @@
+import React, { useState, useEffect } from 'react';
 import { useRoute } from "@react-navigation/native";
-import { View, Text, StyleSheet } from "react-native";
+import { TouchableHighlight, SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import SearchBar from "../Components/SearchBar";
+import { StatusBar } from 'expo-status-bar';
+import * as Font from 'expo-font';
 
 const SearchScreen = () => {
+    const [fontLoaded, setFontLoaded] = useState(false);
     const route = useRoute()
 
     const { title, description } = route.params ? route.params : {}
 
+    useEffect(() => {
+        async function loadFont() {
+            await Font.loadAsync({
+                'DancingScript-Regular': require('../assets/fonts/DancingScript-Regular.ttf'),
+            });
+            setFontLoaded(true);
+        }
+        loadFont();
+    }, []);
+
+    if (!fontLoaded) {
+        return null; // Wait for the font to load before rendering the screen
+    }
+
     return (
-        <View style={styles.container}>
-        <SearchBar />
-            <View style={styles.screen}>
-            
-                <Text>List of Recipes under the category {title}</Text>
-                <Text>{description}</Text>
+        <SafeAreaView>
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>Recipal</Text>
+                </View>
+                <SearchBar />
+                <View style={styles.row}>
+                    <View style={styles.screen}>
+                        <Text>List of Recipes under the category {title}</Text>
+                        <Text>{description}</Text>
+                    </View>
+                    <View style={styles.screens}/>
+                </View>
             </View>
-            <View style={styles.screens}/>
-            <View style={styles.screens}/>
-            <View></View>
-            <View></View>
-            <View></View>
-            <View></View>
-            <View></View>
-            <View></View>
-        </View>
+        </ScrollView>
+        </SafeAreaView>
+        
     );
 }
 
 const styles = StyleSheet.create({
+    header: {
+        paddingTop: 50,
+        paddingBottom: 10,
+        alignItems: 'center',
+    },
+    headerText: {
+        fontFamily: 'DancingScript-Regular',
+        fontWeight: '600',
+        fontSize: 40,
+        color: 'black',
+    },
     screen: {
         padding: 20,
         height: 269,
@@ -36,7 +67,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         marginTop: 10,
         marginStart: 20,
-        backgroundColor: '#FFC800'
+        backgroundColor: '#FFC800',
     },
     screens: {
         padding: 20,
@@ -50,8 +81,11 @@ const styles = StyleSheet.create({
     },
     container: {
         backgroundColor: 'white',
-        flex: 1,
-        paddingTop: 100,
+        height: 1600,
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     },
 })
 
