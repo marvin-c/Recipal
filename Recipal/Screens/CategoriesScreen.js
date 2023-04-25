f0e185708d8110aac317e9e47ea45d02e59daae6
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 <marboynel@gmail.com>
2023-04-25 15:33:27 +1200

    categories api endpoint working. images and title working. added category table and api endpoint on backend

--- Recipal/Screens/CategoriesScreen.js
+++ Recipal/Screens/CategoriesScreen.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import { SafeAreaView, View, Text, ImageBackground, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, ImageBackground, StyleSheet, FlatList, Dimensions, Image } from 'react-native';
 import SearchBar from '../Components/SearchBar';
-import CategoriesList from '../Components/recipes/categories-list';
-import * as Font from 'expo-font';
-import { FlatList } from 'react-native-gesture-handler';
 
 const CategoriesScreen = () => {
+  const [categories, setCategories] = useState([]);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch('http://192.9.174.169/api/categories/');
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const numColumns = 3;
+  const titleWidth = Dimensions.get('window').width / numColumns;
+
+  const renderCategory = ({ item }) => (
+    <View style={styles.category}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <Text style={styles.title}>{item.title}</Text>
+    </View>
+  );
+
   return (
     <SafeAreaView>
       <View style={styles.header}>
         <Text style={styles.headerText}>Recipal</Text>
       </View>
       <SearchBar />
-      <View style={styles.container}>
-        <View style={styles.wrapper}>
-          <View style={{ backgroundColor: 'white' }}>
-          <CategoriesList />
-          </View>
-        </View>
-      </View>
+      <FlatList
+        data={categories}
+        renderItem={renderCategory}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={numColumns}
+        contentContainerStyle={styles.container}
+      />
     </SafeAreaView>
   );
 };
@@ -35,13 +58,30 @@ const styles = StyleSheet.create({
     fontSize: 40,
     color: 'black',
   },
-  wrapper: {
-    flex: 1,
-  },
   container: {
-    flex: 1,
     backgroundColor: 'white',
-    paddingTop: 100,
+    paddingTop: 10,
+    paddingBottom: 100,
+  },
+  category: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+    width: 100,
+    height: 120,
+  },
+  image: {
+    width: 100 - 10,
+    height: 120 - 10,
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+  title: {
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
