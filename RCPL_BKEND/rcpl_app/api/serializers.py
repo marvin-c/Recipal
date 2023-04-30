d5a2bd9a833a2d60022b68cdd3524ddc6467c602
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 <marboynel@gmail.com>
2023-04-30 13:52:15 +1200

    added syncdata.py, renamed some images under assets, put comments on api codes.

--- RCPL_BKEND/rcpl_app/api/serializers.py
+++ RCPL_BKEND/rcpl_app/api/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from ..models import Recipe, Category
 
 
+# This serializer class is used to convert Recipe objects to JSON format and vice versa.
+# It specifies the fields to be included in the serialized output.
 class RecipeModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'title', 'description', 'category', 'ingredients', 'method', 'image', 'date')
 
+# This serializer class is used to convert Category objects to JSON format and vice versa.
+# It specifies the fields to be included in the serialized output.
 class CategoryModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
